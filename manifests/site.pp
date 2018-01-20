be85a8dcaf061196cb639b8b4b34303ec11bad9c
paladox <paladox@users.noreply.github.com>
2018-01-20 13:09:19 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,10 @@ node 'phabricator-2-vm.c.world-wiki.internal' {
     include puppetmaster
 }
 
+node 'ip-172-31-3-168-9a33ab41.c.world-wiki.internal' {
+    include base
+}
+
 # ensures all servers have basic class if puppet runs
 node default {
     include base
