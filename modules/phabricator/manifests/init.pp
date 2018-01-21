77c038e03e8ac8896a4b7ea42f709b3bf5dc1736
paladox <paladox@users.noreply.github.com>
2018-01-21 22:19:31 +0000

    Update init.pp

--- modules/phabricator/manifests/init.pp
+++ modules/phabricator/manifests/init.pp
@@ -67,6 +67,7 @@ class phabricator {
     file { '/srv/phab/phabricator/conf/local/local.json':
         ensure  => present,
         content => template('phabricator/local.json.erb'),
+        require => Service['apache2'],
     }
 
     file { '/srv/phab/images':
@@ -74,8 +75,9 @@ class phabricator {
     }
 
     file { '/etc/php/7.1/apache2/php.ini':
-        ensure => present,
-        mode   => '0755',
-        source => 'puppet:///modules/phabricator/php.ini',
+        ensure  => present,
+        mode    => '0755',
+        source  => 'puppet:///modules/phabricator/php.ini',
+        require => Service['apache2'],
     }
 }
