1ba2d55abc2ede052332ab6a6d85d7a290fea88a
paladox <paladox@users.noreply.github.com>
2018-01-23 23:00:40 +0000

    Update mediawiki.pp

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -6,16 +6,7 @@ class role::mediawiki {
         description => 'MediaWiki server',
     }
 
-    file { '/mnt/mediawiki-static':
+    file { '/srv/mediawiki-static':
         ensure => directory,
-    }
-
-    mount { '/mnt/mediawiki-static':
-        ensure  => mounted,
-        device  => '81.4.124.61:/srv/mediawiki-static',
-        fstype  => 'nfs',
-        options => 'rw',
-        atboot  => true,
-        require => File['/mnt/mediawiki-static'],
     }
 }
