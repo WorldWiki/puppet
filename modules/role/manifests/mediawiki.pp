15100acbad76a1e75c9f24aa4871660bf3dca97a
paladox <paladox@users.noreply.github.com>
2018-01-23 22:35:51 +0000

    Update mediawiki.pp

--- modules/role/manifests/mediawiki.pp
+++ modules/role/manifests/mediawiki.pp
@@ -2,16 +2,6 @@
 class role::mediawiki {
     include ::mediawiki
 
-    ufw::allow { 'http port tcp':
-        proto => 'tcp',
-        port  => 80,
-    }
-
-    ufw::allow { 'https port tcp':
-        proto => 'tcp',
-        port  => 443,
-    }
-
     motd::role { 'role::mediawiki':
         description => 'MediaWiki server',
     }
