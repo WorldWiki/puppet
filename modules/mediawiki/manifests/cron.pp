4135f2e4ab9baa91b6a9d73a99d846cf7827be6c
Reception123 <utilizator.receptie123@gmail.com>
2018-01-24 19:40:42 +0100

    no dblist for worldwiki

--- modules/mediawiki/manifests/cron.pp
+++ modules/mediawiki/manifests/cron.pp
@@ -2,13 +2,6 @@
 #
 # Used for CORE crons which should be ran on every MediaWiki server.
 class mediawiki::cron {
-    cron { 'update_database_lists':
-        ensure  => present,
-        command => '/usr/bin/php /srv/mediawiki/w/extensions/CreateWiki/DBListGenerator.php --wiki metawiki',
-        user    => 'www-data',
-        minute  => '*',
-        hour    => '*',
-    }
     cron { 'update.php for LocalisationUpdate':
         ensure  => present,
         command => '/usr/bin/php /srv/mediawiki/w/extensions/LocalisationUpdate/update.php --wiki extloadwiki > /var/log/mediawiki/debuglogs/l10nupdate.log',
