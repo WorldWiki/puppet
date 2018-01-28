196b6d7d087fa2608a1409ac55ab1add82ba87d3
Paladox <thomasmulhall410@yahoo.com>
2018-01-28 00:07:12 +0000

    Initial import of ircecho

---
+++ modules/ircecho/manifests/init.pp
@@ -0,0 +1,45 @@
+# IRC echo class
+# To use this class, you must define some parameters; here's an example
+# (leading hashes on channel names are added for you if missing):
+# $ircecho_logs = {
+#  "/var/log/nagios/irc.log" =>
+#  ["wikimedia-operations","#wikimedia-tech"],
+#  "/var/log/nagios/irc2.log" => "#irc2",
+# }
+# $ircecho_nick = "icinga-wm"
+# $ircecho_server = "chat.freenode.net"
+class ircecho (
+    $ircecho_logs,
+    $ircecho_nick,
+    $ircecho_server = 'chat.freenode.net',
+    $ensure = 'present',
+) {
+
+    require_package(['python-pyinotify', 'python-irc'])
+
+    file { '/usr/local/bin/ircecho':
+        ensure => 'present',
+        source => 'puppet:///modules/ircecho/ircecho.py',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+    }
+
+    file { '/etc/default/ircecho':
+        ensure  => 'present',
+        content => template('ircecho/default.erb'),
+        owner   => 'root',
+        mode    => '0755',
+        notify  => Service['ircecho'],
+    }
+
+    base::service_unit { 'ircecho':
+        ensure         => $ensure,
+        systemd        => systemd_template('ircecho'),
+        sysvinit       => sysvinit_template('ircecho'),
+        require        => File['/usr/local/bin/ircecho'],
+        service_params => {
+            hasrestart => true,
+        },
+    }
+}
