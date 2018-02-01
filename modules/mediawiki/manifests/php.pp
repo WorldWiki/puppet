df18caf01c54c4f33eead4315752c1322060d2cf
paladox <paladox@users.noreply.github.com>
2018-02-01 17:00:15 +0000

    Update php.pp

--- modules/mediawiki/manifests/php.pp
+++ modules/mediawiki/manifests/php.pp
@@ -1,48 +1,76 @@
 # mediawiki::php
 class mediawiki::php {
     $packages = [
+        'php',
+        'php-curl',
+        'php-fpm',
+        'php-gd',
+        'php-intl',
+        'php-json',
         'php-imagick',
         'php-pear',
         'php-mail',
+        'php-mcrypt',
         'php-mbstring',
         'php-mysql',
         'php-redis',
-        'php7.0',
-        'php7.0-curl',
-        'php7.0-fpm',
-        'php7.0-gd',
-        'php7.0-intl',
-        'php7.0-json',
-        'php7.0-mcrypt',
     ]
 
     package { $packages:
         ensure => present,
     }
 
-    service { 'php7.0-fpm':
-        ensure  => running,
-        require => Package['php7.0-fpm'],
-    }
+    if os_version('ubuntu == xenial') {
+        service { 'php7.0-fpm':
+            ensure  => running,
+            require => Package['php7.0-fpm'],
+        }
 
-    file { '/etc/php/7.0/fpm/php-fpm.conf':
-        ensure => 'present',
-        mode   => '0755',
-        source => 'puppet:///modules/mediawiki/php/php-fpm.conf',
-        notify => Service['php7.0-fpm'],
-    }
+        file { '/etc/php/7.0/fpm/php-fpm.conf':
+            ensure => 'present',
+            mode   => '0755',
+            source => 'puppet:///modules/mediawiki/php/php-fpm.conf',
+            notify => Service['php7.0-fpm'],
+        }
 
-    file { '/etc/php/7.0/fpm/pool.d/www.conf':
-        ensure => 'present',
-        mode   => '0755',
-        source => 'puppet:///modules/mediawiki/php/www.conf',
-        notify => Service['php7.0-fpm'],
-    }
+        file { '/etc/php/7.0/fpm/pool.d/www.conf':
+            ensure => 'present',
+            mode   => '0755',
+            source => 'puppet:///modules/mediawiki/php/www.conf',
+            notify => Service['php7.0-fpm'],
+        }
 
-    file { '/etc/php/7.0/fpm/php.ini':
-        ensure => present,
-        mode   => '0755',
-        source => 'puppet:///modules/mediawiki/php/php.ini',
-        notify => Service['php7.0-fpm'],
+        file { '/etc/php/7.0/fpm/php.ini':
+            ensure => present,
+            mode   => '0755',
+            source => 'puppet:///modules/mediawiki/php/php.ini',
+            notify => Service['php7.0-fpm'],
+        }
+    } else {
+        service { 'php7.1-fpm':
+            ensure  => running,
+            require => Package['php7.1-fpm'],
+        }
+
+        file { '/etc/php/7.1/fpm/php-fpm.conf':
+            ensure => 'present',
+            mode   => '0755',
+            source => 'puppet:///modules/mediawiki/php/php-fpm.conf',
+            notify => Service['php7.1-fpm'],
+        }
+
+        file { '/etc/php/7.1/fpm/pool.d/www.conf':
+            ensure => 'present',
+            mode   => '0755',
+            source => 'puppet:///modules/mediawiki/php/www.conf',
+            notify => Service['php7.1-fpm'],
+        }
+
+        file { '/etc/php/7.1/fpm/php.ini':
+            ensure => present,
+            mode   => '0755',
+            source => 'puppet:///modules/mediawiki/php/php.ini',
+            notify => Service['php7.1-fpm'],
+        }
     }
 }
