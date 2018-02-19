1f71a5b693e4ee7b59bd9577c00feb60a4f19f8e
Devin/Zppix <megadev44s.mail@gmail.com>
2018-02-19 17:49:05 -0600

    Update irc.pp

--- modules/role/manifests/irc.pp
+++ modules/role/manifests/irc.pp
@@ -11,22 +11,22 @@ class role::irc {
         sleeptime    => '0.5',
     }
 
-    ufw::allow { 'ircrcbot port mw1':
-        proto => 'udp',
-        port  => '5070',
-        from  => '185.52.1.75',
-    }
+    #ufw::allow { 'ircrcbot port mw1':
+        #proto => 'udp',
+        #port  => '5070',
+        #from  => '185.52.1.75',
+    #}
 
-    ufw::allow { 'ircrcbot port mw2':
-        proto => 'udp',
-        port  => '5070',
-        from  => '185.52.2.113',
-    }
-    ufw::allow { 'ircrcbot port mw3':
-        proto => 'udp',
-        port  => '5070',
-        from  => '81.4.121.113',
-    }
+    #ufw::allow { 'ircrcbot port mw2':
+     #   proto => 'udp',
+      #  port  => '5070',
+       # from  => '185.52.2.113',
+    #}
+    #ufw::allow { 'ircrcbot port mw3':
+     #   proto => 'udp',
+      #  port  => '5070',
+       # from  => '81.4.121.113',
+    #}
 
     motd::role { '::irc':
         description => 'IRC bots server',
